a644222eb1b7bc2d9eefed79b68fa9b9458d9adb
Samihesam <134228760+Samihesam@users.noreply.github.com>
2026-07-11 22:40:04 +0330

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+// settings.gradle.kts (سطح ریشه‌ی ریپازیتوری)
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AidenAssistant"
+include(":app")
